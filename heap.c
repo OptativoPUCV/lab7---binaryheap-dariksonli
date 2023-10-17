57b0270daf77c40fff761ba1f2f957383d2a5339
Replit user <>
2023-10-17 00:20:06 +0000

    new try

--- heap.c
+++ heap.c
@@ -23,9 +23,11 @@ void* heap_top(Heap* pq)
   return pq->heapArray->data;
 }
 
-void heap_push(Heap* pq, void* data, int priority){
-  if(pq->size >= pq->capac){
-    pq->heapArray = realloc(pq->heapArray, ((pq->capac * 2) + 1) * sizeof(Heap));
+void heap_push(Heap* pq, void* data, int priority)
+{
+  if(pq->size >= pq->capac)
+  {
+    pq->heapArray = realloc(pq->heapArray, ((pq->capac*2)+1)* sizeof(Heap));
     pq->capac = (pq->capac * 2) + 1;
   }
 
@@ -38,7 +40,8 @@ void heap_push(Heap* pq, void* data, int priority){
   void * datoTemp;
   int prioridadTemp;
 
-  while(posicion > 0 && pq->heapArray[posicion].priority > pq->heapArray[padre].priority){
+  while(posicion > 0 && pq->heapArray[posicion].priority > pq->heapArray[padre].priority)
+  {
     datoTemp = pq->heapArray[posicion].data;
     pq->heapArray[posicion].data = pq->heapArray[padre].data;
     pq->heapArray[padre].data = datoTemp;
@@ -52,54 +55,14 @@ void heap_push(Heap* pq, void* data, int priority){
   }
 }
 
-/*void heap_push(Heap* pq, void* data, int priority)
-{
-  int indice = pq->size;
-
-  //si esta vacio insertar altiro
-  if(indice == 0){
-    pq->heapArray[indice].data = data;
-    pq->heapArray[indice].priority = priority;
-    pq->size++;
-    return;
-  }
-  //si arreglo esta lleno aumentar capacidad
-  if(pq->capac == pq->size)
-  {
-    int capacidad = (pq->capac * 2)+1;
-    pq->heapArray = (heapElem *) realloc(pq->heapArray , sizeof(int ) * capacidad);
-    if(pq->heapArray == NULL) return;
-  }
-
-  pq->size++;
-  pq->heapArray[indice].data = data;
-  pq->heapArray[indice].priority = priority;
-
-  if(pq->heapArray[indice].priority < pq->heapArray[pq->size].priority)//si el nuevo elemento es menor que el padre
-  {
-    heapElem aux = pq->heapArray[indice];//se guarda el elemento en aux
-    pq->heapArray[indice] = pq->heapArray[indice];//se guarda el nuevo elemento en el padre
-    pq->heapArray[indice] = aux;//se guarda el aux en el padre
-    indice = (indice-1)/2;//se vuelve a llamar a la funcion con el nuevo indice
-    heap_push(pq, data, priority);
-    return;
-  }
-  
-  while(pq->heapArray[indice].priority > pq->heapArray[(indice-1)/2].priority)
-  {
-    heapElem aux = pq->heapArray[indice];
-    pq->heapArray[indice] = pq->heapArray[(indice-1)/2];
-    pq->heapArray[(indice-1)/2] = aux;
-    indice = (indice-1)/2;
-    if(indice == 0) break;
-  }
-  
-  
-}
-*/
-
 void heap_pop(Heap* pq)
 {
+  if(pq->size == 0) return;
+
+  pq->heapArray[0].data = pq->heapArray[pq->size - 1].data;
+  pq->heapArray[0].priority = pq->heapArray[pq->size - 1].priority;
+  pq->size--;
+
   void * aux = pq->heapArray[pq->size].data; //se guarda el ultimo elemento en aux
   pq->heapArray[0].data = aux;
   pq->heapArray[pq->size].data = NULL;
@@ -108,7 +71,7 @@ void heap_pop(Heap* pq)
   int indice = 0;
   int hijoIzq = (indice*2)+1;
   int hijoDer = (indice*2)+2;
-  
+  //
   while(pq->heapArray[indice].priority < pq->heapArray[hijoIzq].priority || pq->heapArray[indice].priority < pq->heapArray[hijoDer].priority)
   {
     if(pq->heapArray[hijoIzq].priority > pq->heapArray[hijoDer].priority)
